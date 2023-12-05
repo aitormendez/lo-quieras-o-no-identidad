e615a4dd1bbb82b891b88e466a079fad88f9fb0d
aitor <aitor@Mac-Studio-de-aitor.local>
2023-12-05 15:21:48 +0100

    debug

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,6 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
-  output: "static",
+  output: "hybrid",
   adapter: vercel(),
 });
