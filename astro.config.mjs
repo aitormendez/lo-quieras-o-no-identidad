85a625723bbce1a49c1de30f2c9cb9073622a6c3
aitor <aitor@Mac-Studio-de-aitor.local>
2023-12-05 14:58:20 +0100

    vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
-import vercelStatic from "@astrojs/vercel/static";
+// import vercelStatic from "@astrojs/vercel/static";
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
   output: "static",
-  adapter: vercelStatic(),
+  adapter: vercel(),
 });
